6900bafa9f34fa0bd47d1e6699635e572badfbdb
swetaraj4194 <swetaraj4194@gmail.com>
2022-03-29 11:54:10 +0200

    created all file & initialize the reducer

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { combineReducers, createStore } from "redux";
+import userReducer from "./user/reducer";
+import pizzasReducer from "./pizzas/reducer";
+
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (x) => x;
+
+const store = createStore(
+  combineReducers({
+    user: userReducer,
+    pizzas: pizzasReducer,
+  }),
+  enhancer
+);
+
+export default store;
---
+++ src/store/user/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  name: "sweta",
+  id: 42,
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
